8413b9829b2e5dc1724e9f498e0e9c0e9b2c529e
Pranjalbajpai3030 <127866867+Pranjalbajpai3030@users.noreply.github.com>
2024-06-26 18:24:18 +0530

    made some ui changes

--- src/component/News.js
+++ src/component/News.js
@@ -19,7 +19,7 @@ const News = (props) => {
 
    const fetchMoreData = async () => {
       const nextPage = page + 1;
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7c7c798ba157455299775fe83f2d8725&page=${nextPage}&pageSize=${props.pageSize}`;
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=3027806966fc4a6daf7688dd944eef6f&page=${nextPage}&pageSize=${props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json();
       setArticles(articles.concat(parsedData.articles));
@@ -29,7 +29,7 @@ const News = (props) => {
 
    const updateNews = async () => {
       props.setProgress(10);
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7c7c798ba157455299775fe83f2d8725&page=${page}&pageSize=${props.pageSize}`;
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=3027806966fc4a6daf7688dd944eef6f&page=${page}&pageSize=${props.pageSize}`;
       setLoading(true);
       let data = await fetch(url);
       props.setProgress(30);
@@ -53,8 +53,13 @@ const News = (props) => {
 
    return (
       <div className="container my-3">
+        <div className="container text-center mb-4">
+    <img src="logo.png" alt="Logo" className="img-fluid" style={{ maxWidth: '500px', marginTop: '70px' }} />
+</div>
+
+
          <h1 style={{ marginTop: '70px', marginBottom: '35px', color: '#3D5280', transition: 'color 0.5s ease', textAlign: 'center' }}>
-            News Xpress - <span style={{ color: 'red' }}>{props.category}</span> Top News
+            <span style={{ color: 'red' }}>{props.category}</span> Top News
          </h1>
          {loading && <Spinner />}
          <InfiniteScroll
