bfba31bd05553ca8b2bba974e9484fe181a21d11
Pranjalbajpai3030 <127866867+Pranjalbajpai3030@users.noreply.github.com>
2024-07-09 20:52:57 +0530

    some logo changes

--- src/component/News.js
+++ src/component/News.js
@@ -14,49 +14,46 @@ const News = (props) => {
    useEffect(() => {
       document.title = `${props.category} - News Express`;
       updateNews();
-      // eslint-disable-next-line no-console,
    }, []);
 
    const fetchMoreData = async () => {
       const nextPage = page + 1;
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=3027806966fc4a6daf7688dd944eef6f&page=${nextPage}&pageSize=${props.pageSize}`;
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      setArticles(articles.concat(parsedData.articles));
-      setTotalResults(parsedData.totalResults);
-      setPage(nextPage);
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7c7c798ba157455299775fe83f2d8725&page=${nextPage}&pageSize=${props.pageSize}`;
+      try {
+         let data = await fetch(url);
+         let parsedData = await data.json();
+         setArticles([...articles, ...parsedData.articles]);
+         setTotalResults(parsedData.totalResults);
+         setPage(nextPage);
+      } catch (error) {
+         console.error('Error fetching more news:', error);
+      }
    };
 
    const updateNews = async () => {
       props.setProgress(10);
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=3027806966fc4a6daf7688dd944eef6f&page=${page}&pageSize=${props.pageSize}`;
-      setLoading(true);
-      let data = await fetch(url);
-      props.setProgress(30);
-      let parsedData = await data.json();
-      props.setProgress(50);
-      setArticles(parsedData.articles);
-      setTotalResults(parsedData.totalResults);
-      setLoading(false);
-      props.setProgress(100);
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7c7c798ba157455299775fe83f2d8725&page=${page}&pageSize=${props.pageSize}`;
+      try {
+         setLoading(true);
+         let data = await fetch(url);
+         props.setProgress(30);
+         let parsedData = await data.json();
+         props.setProgress(50);
+         setArticles(parsedData.articles);
+         setTotalResults(parsedData.totalResults);
+         setLoading(false);
+         props.setProgress(100);
+      } catch (error) {
+         console.error('Error updating news:', error);
+         setLoading(false);
+      }
    };
-
-   /*const handlePrevClick = async () => {
-      setPage(page - 1);
-      updateNews();
-   };
-
-   const handleNextClick = async () => {
-      setPage(page + 1);
-      updateNews();
-   };*/
 
    return (
       <div className="container my-3">
-        <div className="container text-center mb-4">
-    <img src="logo.png" alt="Logo" className="img-fluid" style={{ maxWidth: '500px', marginTop: '70px' }} />
-</div>
-
+         <div className="container text-center mb-4">
+            <img src="logo.png" alt="Logo" className="img-fluid" style={{ maxWidth: '500px', marginTop: '70px' }} />
+         </div>
 
          <h1 style={{ marginTop: '70px', marginBottom: '35px', color: '#3D5280', transition: 'color 0.5s ease', textAlign: 'center' }}>
             <span style={{ color: 'red' }}>{props.category}</span> Top News
